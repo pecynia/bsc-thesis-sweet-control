7b7005f28fa7833ba7adedce559e6a02333d706b
pecynia <73779040+pecynia@users.noreply.github.com>
2023-06-21 16:21:27 +0200

    Update enums.py
    Removed personal pc values

--- enums.py
+++ enums.py
@@ -2,19 +2,19 @@ import json
 import pygame
 import python.data_processing.distributions as distributions
 
-DATA_PATH_OLD = "C:/Users/verhe/OneDrive/Uni/bachelor thesis/glucose/glucose_15min.xlsx"
-DATA_PATH = "C:/Users/verhe/OneDrive/Uni/bachelor thesis/data/glucose_15min.xlsx"
-NEW_DATA_PATH = "C:/Users/verhe/OneDrive/Uni/bachelor thesis/python/visualization/data/data_2.csv"
+DATA_PATH_OLD = ""
+DATA_PATH = ""  # Where to import from
+NEW_DATA_PATH = ""  # Where to put the processed data
 
 #  ------------------------------------------------------------ Data  --------------------------------------------------------------------------------
 
 # Insulin activity, if changed, reprocess the data
 INSULIN_ACTIVITY_FUNCTION = distributions.gamma
-INSULIN_ACTIVITY_DELAY = 0  # 20 minutes
+INSULIN_ACTIVITY_DELAY = 0  # 12.28 minutes, we already incorperate it in the activation function
 INSULIN_STEP_SIZE = 0.05
 INSULIN_LOW = 3
 INSULIN_HIGH = 10
 GLUCOSE_BASELINE_LEVEL = 4.5  # mmol/L
 
-with open('C:/Users/verhe/OneDrive/Uni/bachelor thesis/python/visualization/python/data_processing/config.json', 'r') as json_file:
+with open('./config.json', 'r') as json_file:
     json_load = json.load(json_file)
